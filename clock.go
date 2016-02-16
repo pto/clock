bac4607c1871a6c0938faa380dacf99e502810e4
Peter Olsen <pto@me.com>
2016-02-16 18:54:34 -0500

    temp target

--- clock.go
+++ clock.go
@@ -20,8 +20,8 @@ func main() {
 		highlight_start = "\x1b[1;36m"
 		highlight_end   = "\x1b[0m"
 	)
-	fmt.Print(indent, highlight_start, "Just Go", highlight_end, "\n")
-	target := time.Date(2016, 1, 1, 0, 0, 0, 0, time.UTC)
+	fmt.Print(indent, highlight_start, "Go 1.6", highlight_end, "\n")
+	target := time.Date(2016, 2, 17, 0, 0, 0, 0, time.Local)
 	fmt.Print(indent, target.Format(time.UnixDate), "\n")
 
 	var previous time.Time
