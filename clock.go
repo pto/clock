956351cd19a2852ab7417f568010a1390530db1a
Peter Olsen <pto@me.com>
2016-04-15 19:15:52 -0400

    2^4

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2016, 4, 12, 22, 0, 0, 0, time.Local)
+	target := time.Date(2016, 4, 16, 0, 0, 0, 0, time.Local)
 	printTargetTime(target)
 	exitOnEnterKey()
 
