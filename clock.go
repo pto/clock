da8895622bb5f33b963c6f95255ce99d04fc9f5f
Peter Olsen <pto@me.com>
2016-06-04 07:13:48 -0400

    local time

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2016, 6, 4, 0, 0, 0, 0, time.UTC)
+	target := time.Date(2016, 6, 4, 0, 0, 0, 0, time.Local)
 	printTargetTime(target)
 	exitOnEnterKey()
 
