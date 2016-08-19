4acebe249c91af2884f5fab41be10a34d86a93e0
Peter Olsen <pto@me.com>
2016-08-19 18:43:37 -0400

    Local time

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2016, 8, 20, 0, 0, 0, 0, time.UTC)
+	target := time.Date(2016, 8, 20, 0, 0, 0, 0, time.Local)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
