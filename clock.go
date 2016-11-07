183fee0587d6cf470acef3cb307d6328206a2c54
Peter Olsen <pto@me.com>
2016-11-06 20:42:20 -0500

    Golang 7th anniversary

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2016, 10, 1, 0, 0, 0, 0, time.Local)
+	target := time.Date(2016, 11, 10, 0, 0, 0, 0, time.Local)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
