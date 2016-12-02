cb3828a8e4028e5942691e8d14df652bf250471b
Peter Olsen <pto@me.com>
2016-12-01 19:53:54 -0500

    reset to midnight

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2016, 12, 1, 11, 0, 0, 0, time.Local)
+	target := time.Date(2016, 12, 1, 0, 0, 0, 0, time.Local)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
