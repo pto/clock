a8e521d36221150eb74c7fdbeb123b3d6cba831c
Peter Olsen <pto@me.com>
2016-12-09 20:15:36 -0500

    Solstice

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2016, 12, 14, 0, 5, 0, 0, time.UTC)
+	target := time.Date(2016, 12, 21, 5, 44, 8, 0, time.Local)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
