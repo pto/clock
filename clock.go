0d76a11098ac85e1613c228bde0caf6ee3884585
Peter Olsen <pto@me.com>
2017-03-11 08:34:18 -0500

    Daylight Saving Time

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2017, 2, 16, 0, 0, 0, 0, time.Local)
+	target := time.Date(2017, 3, 12, 3, 0, 0, 0, time.Local)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
