25caccf44f14ffd5d22546c227c4ecd5c577f2df
Peter Olsen <pto@me.com>
2017-11-18 07:45:44 -0500

    Honoring PDP-11/20

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2017, 11, 10, 0, 0, 0, 0, time.Local)
+	target := time.Date(2017, 11, 20, 0, 0, 0, 0, time.UTC)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
