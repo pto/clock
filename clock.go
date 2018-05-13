0a08bf3d09d321a3950f907817e3d347292d7bce
Peter Olsen <pto@me.com>
2018-05-12 21:35:01 -0400

    reset

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2018, 5, 12, 0, 0, 0, 0, time.Local)
+	target := time.Date(2018, 5, 12, 21, 36, 0, 0, time.Local)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
