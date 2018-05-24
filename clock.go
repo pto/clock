d63a813d269f6327ad7565474d3a607177f64d5c
Peter Olsen <pto@me.com>
2018-05-24 10:34:41 -0400

    Unofficial start of summer

--- clock.go
+++ clock.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	target := time.Date(2018, 5, 21, 0, 0, 0, 0, time.UTC)
-	motto := "Go Simplify"
+	target := time.Date(2018, 5, 26, 0, 0, 0, 0, time.Local)
+	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
 
