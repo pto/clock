210cfa4a53cf8694d232cf64bbcce867907fc5dd
Peter Olsen <pto@me.com>
2018-05-25 21:36:15 -0400

    remove vim demo

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target = time.Date(2018, 5, 26, 0, 0, 0, 0, time.Local)
+	target := time.Date(2018, 5, 26, 0, 0, 0, 0, time.Local)
 	motto := "Simply Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
