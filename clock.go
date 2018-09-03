60029874dcff8c0082fc9762455172d23138816a
Peter Olsen <pto@me.com>
2018-09-03 19:42:00 -0400

    UTC

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2018, 9, 4, 0, 0, 0, 0, time.Local)
+	target := time.Date(2018, 9, 4, 0, 0, 0, 0, time.UTC)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
