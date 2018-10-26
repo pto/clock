ebc610fe5b3c31a025fb33674965f21e2489fe1d
Peter Olsen <pto@me.com>
2018-10-26 19:21:19 -0400

    8-week drill

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2018, 10, 24, 0, 0, 0, 0, time.UTC)
+	target := time.Date(2018, 10, 28, 0, 0, 0, 0, time.UTC)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
