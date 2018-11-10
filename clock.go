8765071b8a8b79d6db794905e94c7d244a0fbd9b
Peter Olsen <pto@me.com>
2018-11-09 19:49:08 -0500

    Starting Go's 10th year

--- clock.go
+++ clock.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	target := time.Date(2018, 11, 2, 18, 6, 29, 0, time.Local)
+	target := time.Date(2018, 11, 10, 0, 0, 0, 0, time.Local)
 	motto := "Just Go"
 	printTargetTime(target, motto)
 	exitOnEnterKey()
