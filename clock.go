04fb9d9916133ae63f71a4ab64ed123048f8cbfc
Peter Olsen <pto@me.com>
2019-04-29 19:46:37 -0400

    May Day

--- clock.go
+++ clock.go
@@ -9,8 +9,8 @@ import (
 
 // Update target and motto as desired.
 var (
-	target = time.Date(2019, 4, 22, 0, 0, 0, 0, time.UTC)
-	motto  = "Simply Go"
+	target = time.Date(2019, 5, 1, 0, 0, 0, 0, time.UTC)
+	motto  = "Just Go"
 )
 
 func main() {
