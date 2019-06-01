6db483816805f3ac9f6dbb01478d46ae1a0a1991
Peter Olsen <pto@me.com>
2019-05-31 20:15:33 -0400

    Simple restart

--- clock.go
+++ clock.go
@@ -9,8 +9,8 @@ import (
 
 // Update target and motto as desired.
 var (
-	target = time.Date(2019, 5, 27, 0, 0, 0, 0, time.UTC)
-	motto  = "Summer of Go"
+	target = time.Date(2019, 6, 1, 0, 0, 0, 0, time.Local)
+	motto  = "Simplify"
 )
 
 func main() {
