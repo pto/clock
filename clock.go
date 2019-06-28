95e4b5dcec3f99155284e749f6b43482997a9a76
Peter Olsen <pto@me.com>
2019-06-27 21:31:21 -0400

    2 * math.Pi day

--- clock.go
+++ clock.go
@@ -9,7 +9,7 @@ import (
 
 // Update target and motto as desired.
 var (
-	target = time.Date(2019, 7, 1, 0, 0, 0, 0, time.Local)
+	target = time.Date(2019, 6, 28, 0, 0, 0, 0, time.Local)
 	motto  = "Simplify"
 )
 
