e5948873b39ded4f8cad54f29cd3e27b22dca3d8
Peter Olsen <pto@me.com>
2019-09-22 21:57:30 -0400

    Equinox

--- clock.go
+++ clock.go
@@ -9,7 +9,7 @@ import (
 
 // Update target and motto as desired.
 var (
-	target = time.Date(2019, 9, 1, 0, 0, 0, 0, time.Local)
+	target = time.Date(2019, 9, 23, 3, 50, 51, 0, time.Local)
 	motto  = "Just Go"
 )
 
