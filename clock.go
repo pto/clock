3b8441a32e801d1acac1a57122ed9d1ed76e3009
Peter Olsen <pto@me.com>
2020-04-15 19:10:10 -0400

    2^2/2^4

--- clock.go
+++ clock.go
@@ -9,7 +9,7 @@ import (
 
 // Update target and motto as desired.
 var (
-	target = time.Date(2020, 4, 1, 0, 0, 0, 0, time.Local)
+	target = time.Date(2020, 4, 16, 0, 0, 0, 0, time.Local)
 	motto  = "Just Go"
 )
 
